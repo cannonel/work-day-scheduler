c5f30626465abc80061cd28adbb2abd1c11c10fb
Erika Cannon <cannone.rva@gmail.com>
2022-04-01 09:21:14 -0400

    add local storage function, saves and retrieves tasks

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+var userBlocks = [];
 var timeBlocks = [
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-    "16:00",
-    "17:00"
+    "09:00 am",
+    "10:00 am",
+    "11:00 am",
+    "12:00 pm",
+    "1:00 pm",
+    "2:00 pm",
+    "3:00 pm",
+    "4:00 pm",
+    "5:00 pm"
 ];
 
 var contentContainer = document.querySelector("#container");
@@ -17,11 +18,13 @@ var currentTime = moment().format("HH") + ":00";
 
 //get current time and display in header
 window.setInterval(function() {
-    $("#currentDay").text(moment().format("ddd MM/DD h:mm:ss a"));
+    $("#currentDay").text(moment().format("ddd MM/DD h:mm a"));
 },1000);
 
 //getting main content, time/tasks/save
 var showHourBlocks = function() {
+    //call in tasks from local storage
+    localTasks();
     //create loop to run through timeblocks array
     for (i=0; i < timeBlocks.length; i++){
 
@@ -45,6 +48,11 @@ var showHourBlocks = function() {
         taskInput.id = "input" + timeBlocks.indexOf(timeBlocks[i]);
 
         //check local storage in an 'if' statement
+        if (userBlocks[i]) {
+            taskInput.value = userBlocks[i];
+          }
+          taskRow.appendChild(taskInput);
+        
 
         //appending task text input to correct row
         taskRow.appendChild(taskInput);
@@ -60,8 +68,36 @@ var showHourBlocks = function() {
 
         //appending dynamically created row element to main content container
         contentContainer.appendChild(taskRow);
+
+        if (currentTime === timeBlocks[i]) {
+            taskInput.classList = "present col-md-9 description p-0"
+        }
+        if (currentTime > timeBlocks[i]) {
+            taskInput.classList = "past col-md-9 description p-0"
+        }
+        if (currentTime < timeBlocks[i]) {
+            taskInput.classList = "future col-md-9 description p-0"
+        }
     }
 
 };
 
 showHourBlocks();
+
+//save button
+$("button").on("click", function () {
+    var newTask = [];
+    for (var i = 0; i < timeBlocks.length; i++){
+        newTask.push(document.getElementsByTagName("input")[i].value);
+    }
+    userBlocks = newTask;
+    localStorage.setItem("tasks", JSON.stringify(userBlocks));
+});
+
+//add function to grab from local storage, if any
+function localTasks() {
+    if (JSON.parse(localStorage.getItem("tasks"))){
+        userBlocks = JSON.parse(localStorage.getItem("tasks"));
+    }
+}
+
